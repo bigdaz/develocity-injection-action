c5d4bf66f1935bcc8cf5cd760dee4acf1c3907dd
daz <daz@gradle.com>
2024-07-21 19:03:04 -0600

    Capture build scan link in Gradle

--- dv-inject-maven/resources/develocity-custom-user-data.groovy
+++ dv-inject-maven/resources/develocity-custom-user-data.groovy
@@ -1,11 +1,10 @@
+// Write build scan links to GitHub Actions job summary
 buildScan.buildScanPublished { published ->
     def buildScanUri = published.buildScanUri
-    log.info("Build scan published at ${buildScanUri}")
+    log.info("Captured build scan link: ${buildScanUri}")
 
     def jobSummaryFile = System.getenv("GITHUB_STEP_SUMMARY")
     if (jobSummaryFile) {
         new File(jobSummaryFile) << "Develocity Build Scan: ${buildScanUri}"
-    } else {
-        log.warn("Could not find environment variable GITHUB_STEP_SUMMARY to write build scan URI to.")
     }
 }
